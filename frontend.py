67b5a13662fd531650c4671e9567d4b2fa727a4f
June Lee <104660209+Myuku@users.noreply.github.com>
2022-04-29 19:01:57 -0700

    change3

--- frontend.py
+++ frontend.py
@@ -6,11 +6,11 @@ from kivymd.uix.textfield import MDTextField
 from kivymd.uix.label import MDLabel
 from kivymd.uix.toolbar import MDToolbar
 
-class ConverterApp(MDApp):
+class MountainApp(MDApp):
     def build(self):
         screen = MDScreen()
         #UI Widgets go here
         return screen
 
 if __name__ == '__main__':
-    ConverterApp().run()
+    MountainApp().run()
