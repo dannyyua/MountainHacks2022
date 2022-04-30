29f0aca934f96b0109367ec54d4ec1b8aec51c79
dannyyua <dannyyua@gmail.com>
2022-04-30 00:38:23 -0700

    edited py files, added imgs

--- game_logic.py
+++ game_logic.py
@@ -1,20 +1,5 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.points = 0
-        self.ready = False
 
-class Guess:
-    def __init__(self, playerID, altitude, prominence, isolation):
-        self.playerID = playerID
-        self.altitude = altitude
-        self.prominence = prominence
-        self.isolation = isolation
 
-class Mountain:
-    def __init__(self, name, altitude, prominence, isolation):
-        self.name = name
-        self.altitude = altitude
-        self.prominence = prominence
-        self.isolation = isolation
-        
+# mFile = open("database.csv")
+
+# for line in mFile:
